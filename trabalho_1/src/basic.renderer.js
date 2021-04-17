9223d5815db489b79319b1580e402af857b374db
Gabriel Christo <christo@dcc.ufrj.br>
2021-04-17 20:29:23 -0300

    rectangle class definition

--- trabalho_1/src/basic.renderer.js
+++ trabalho_1/src/basic.renderer.js
@@ -14,6 +14,16 @@
             this.y = y;
         }
     }
+
+    // rectangle class definition
+    class Rectangle {
+        constructor(x1, x2, y1, y2) {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+    }
         
     function inside(  x, y, primitive  ) {
             // You should implement your inside test here for all shapes   
